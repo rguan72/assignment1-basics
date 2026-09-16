f4491ef96b0f9bfd5d43c7f5e26775391dac65a9
Richard Guan <rguan72@gmail.com>
2026-09-15 19:02:14 -0700

    implement efficient merge

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -1,3 +1,5 @@
+import heapq
+from dataclasses import dataclass
 from git import PathLike
 import os
 from typing import BinaryIO
@@ -10,6 +12,26 @@ import resource
 import sys
 
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
+@dataclass 
+class BytePairWithCount:
+    count: int
+    pair: tuple[bytes, bytes]
+
+    def __lt__(self, other):
+        if self.count == other.count:
+            return self.pair > other.pair
+        return self.count > other.count
+
+@dataclass
+class CountWithMergedPretoken:
+    count: int
+    merged_pretoken: tuple[bytes, ...]
+
+@dataclass
+class CountWithIds:
+    count: int
+    pretoken_original_ids: set[int]
 
 def pretokenize(input_path: str | PathLike, start: int, end: int, special_tokens: list[str]) -> dict[tuple[bytes, ...], int]:
     with open(input_path, "rb") as f:
@@ -25,8 +47,6 @@ def pretokenize(input_path: str | PathLike, start: int, end: int, special_tokens
         return pretokens
 
 def train_bpe(input_path: str | PathLike, vocab_size: int, special_tokens: list[str]) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
-    # TODOS
-    # merge efficiency: don't have to merge over the entire vocab? potentially?
     start_time = time.perf_counter()
     num_processes = 4
     with open(input_path, "rb") as f:
@@ -37,25 +57,74 @@ def train_bpe(input_path: str | PathLike, vocab_size: int, special_tokens: list[
     with Pool(processes=num_processes) as pool:
         pretoken_chunks = pool.starmap(pretokenize, args_list)
 
-    pretokens = copy.deepcopy(pretoken_chunks[0])
+    pretokens_unindexed = copy.deepcopy(pretoken_chunks[0])
     for pretoken_chunk in pretoken_chunks[1:]:
         for pretoken, count in pretoken_chunk.items():
-            pretokens[pretoken] += count
+            pretokens_unindexed[pretoken] += count
+    pretokens: dict[int, CountWithMergedPretoken] = {}
+    for i, (pretoken, count) in enumerate(pretokens_unindexed.items()):
+        pretokens[i] = CountWithMergedPretoken(count=count, merged_pretoken=pretoken)
 
     mid_time = time.perf_counter()
     print(f"pretokenization done. time (s): {mid_time - start_time}")
     print(f"peak memory usage in MB after pretokenization: {peak_rss_mib()}")
 
+    pair_counts = get_stats(pretokens)
+    pair_heap = [BytePairWithCount(count=count_with_ids.count, pair=pair) for pair, count_with_ids in pair_counts.items()]
+    heapq.heapify(pair_heap)
+
     merges: list[tuple[bytes, bytes]] = []
     vocab = create_initial_vocab(special_tokens)
     num_merges = vocab_size - len(vocab)
     for _ in range(num_merges):
-        pairs = get_stats(pretokens)
-        max_pair = max(pairs.items(), key=lambda item: (item[1], item[0]))[0]
+        max_pair = None
+        while max_pair is None:
+            top = heapq.heappop(pair_heap)
+            if top.pair in pair_counts and pair_counts[top.pair].count == top.count:
+                max_pair = top.pair
+        pretokens_affected_ids = pair_counts[max_pair].pretoken_original_ids.copy()
+        byte_pairs_updated: dict[tuple[bytes, bytes], int] = collections.defaultdict(int)
+        for pretoken_id in pretokens_affected_ids:
+            pretoken = pretokens[pretoken_id].merged_pretoken
+            count = pretokens[pretoken_id].count
+            pretoken_out = ()
+            i = 0
+
+            # construct new pretoken with merging
+            while i < len(pretoken):
+                if i+1 < len(pretoken) and (pretoken[i], pretoken[i+1]) == max_pair:
+                    pretoken_out += (pretoken[i]+pretoken[i+1],)
+                    i += 2
+                else:
+                    pretoken_out += (pretoken[i],)
+                    i += 1
+
+            # diff old and new to get count updates. batch together for heap update.
+            for i in range(len(pretoken)-1):
+                byte_pairs_updated[pretoken[i],pretoken[i+1]] -= count
+                pair_counts[pretoken[i],pretoken[i+1]].count -= count
+                pair_counts[pretoken[i],pretoken[i+1]].pretoken_original_ids.discard(pretoken_id)
+            for i in range(len(pretoken_out)-1):
+                byte_pairs_updated[pretoken_out[i],pretoken_out[i+1]] += count
+                if (pretoken_out[i],pretoken_out[i+1]) in pair_counts:
+                    pair_counts[pretoken_out[i],pretoken_out[i+1]].count += count
+                    pair_counts[pretoken_out[i],pretoken_out[i+1]].pretoken_original_ids.add(pretoken_id)
+                else:
+                    pair_counts[pretoken_out[i],pretoken_out[i+1]] = CountWithIds(count=count, pretoken_original_ids=set([pretoken_id]))
+
+            # now we can discard the old, unmerged pretoken
+            pretokens[pretoken_id].merged_pretoken = pretoken_out
+
+        # the pair has been replaced everywhere
+        del pair_counts[max_pair]
+        for byte_pair, count in byte_pairs_updated.items():
+            if byte_pair in pair_counts and count != 0:
+                heapq.heappush(pair_heap, BytePairWithCount(count=pair_counts[byte_pair].count, pair=byte_pair))
+
         merges.append(max_pair)
-        pretokens = merge_vocab(max_pair, pretokens)
         new_idx = len(vocab)
         vocab[new_idx] = max_pair[0] + max_pair[1]
+
     end_time = time.perf_counter()
     print(f"merge done. time (s): {end_time - start_time}")
     print(f"peak memory usage in MB after merging: {peak_rss_mib()}")
@@ -70,11 +139,16 @@ def create_initial_vocab(special_tokens: list[str]) -> dict[int, bytes]:
         vocab[256 + j] = special_token.encode("utf-8")
     return vocab
 
-def get_stats(vocab: dict[tuple[bytes, ...], int]) -> dict[tuple[bytes, bytes], int]:
-    pairs = collections.defaultdict(int)
-    for pretoken, freq in vocab.items():
+def get_stats(vocab: dict[int, CountWithMergedPretoken]) -> dict[tuple[bytes, bytes], CountWithIds]:
+    pairs: dict[tuple[bytes, bytes], CountWithIds] = {}
+    for pretoken_original_id, count_with_merged_pretoken in vocab.items():
+        pretoken = count_with_merged_pretoken.merged_pretoken
         for i in range(len(pretoken) - 1):
-            pairs[pretoken[i],pretoken[i+1]] += freq
+            if (pretoken[i], pretoken[i+1]) in pairs:
+                pairs[pretoken[i],pretoken[i+1]].count += count_with_merged_pretoken.count
+                pairs[pretoken[i],pretoken[i+1]].pretoken_original_ids.add(pretoken_original_id)
+            else:
+                pairs[pretoken[i],pretoken[i+1]] = CountWithIds(count=count_with_merged_pretoken.count, pretoken_original_ids=set([pretoken_original_id]))
     return pairs
 
 def merge_vocab(pair: tuple[bytes, bytes], v_in: dict[tuple[bytes, ...], int]) -> dict[tuple[bytes, ...], int]:
